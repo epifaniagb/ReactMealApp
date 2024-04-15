836827c0d6de2335c184e3f1f249da8e73dac4f7
epifaniagb <epifaniaegb@gmail.com>
2024-04-15 16:26:13 -0400

    playing w navbar

--- MealApp/src/components/NavBar.jsx
+++ MealApp/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (<nav>
-    <ul>
+    <ul className="navBar">
       <li><Link to='/about'>About</Link></li>
       <li><Link to='/'>Home</Link></li>
     </ul>
