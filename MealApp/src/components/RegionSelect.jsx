305600d1831f5e178e388d1dc5d52f4d9d13c93e
epifaniagb <epifaniaegb@gmail.com>
2024-04-16 07:30:07 -0400

    dropdown menu attempt?

--- MealApp/src/components/RegionSelect.jsx
+++ MealApp/src/components/RegionSelect.jsx
@@ -2,31 +2,39 @@ import React, { useEffect, useState } from 'react';
 import fetchData from '../utils/fetchData';
 
 const RegionComponent = () => {
-    const [regions, setRegions] = useState([]);
-  
-    useEffect(() => {
-      const fetchRegions = async () => {
-        const eachRegion = await fetchData('https://www.themealdb.com/api/json/v1/1/list.php?a=list');
-        const fiveRegions = eachRegion[0].meals.slice(0, 5);
-        setRegions(fiveRegions);
-        console.log(eachRegion);
-        console.log(fiveRegions);
-        console.log(regions);
-      };
-  
-      fetchRegions();
-    }, []);
-  
-    return (
-      <div className="dropdown-menu">
-        {regions.map(region => (
-          <li key={region.strArea}>{region.strArea}</li>
-        ))}
-      </div>
-    );
+  const [regions, setRegions] = useState([]);
+  const [selectedRegion, setSelectedRegion] = useState('');
+
+  useEffect(() => {
+    const fetchRegions = async () => {
+      const eachRegion = await fetchData('https://www.themealdb.com/api/json/v1/1/list.php?a=list');
+      const fiveRegions = eachRegion[0].meals.slice(0, 5);
+      setRegions(fiveRegions);
+    };
+
+    fetchRegions();
+  }, []);
+
+  const handleRegionChange = (e) => {
+    setSelectedRegion(e.target.value);
   };
 
-  export default RegionComponent;
+  return (
+    <div className="dropdown-menu">
+      <select value={selectedRegion} onChange={handleRegionChange}>
+        <option value="">Select Region</option>
+        {regions.map(region => (
+          <option key={region.strArea} value={region.strArea}>{region.strArea}</option>
+        ))}
+      </select>
+      {selectedRegion && (
+        <p>You selected: {selectedRegion}</p>
+      )}
+    </div>
+  );
+};
+
+export default RegionComponent;
 
   // fetch function utils
   // refactor to be a form
