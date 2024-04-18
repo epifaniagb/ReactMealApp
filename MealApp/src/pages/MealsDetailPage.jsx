9b397bcd9884994f5ee031b1c6e146dfb36f2f39
epifaniagb <epifaniaegb@gmail.com>
2024-04-18 16:02:46 -0400

    building context

--- MealApp/src/pages/MealsDetailPage.jsx
+++ MealApp/src/pages/MealsDetailPage.jsx
@@ -1,11 +1,14 @@
 import MealDetails from "../components/MealDetails";
-
+import NavBar from "../components/NavBar";
 const MealDetailsPage = () => {
 
     return (
-
+        <>
+         <NavBar/>
         <MealDetails/>
+        </>
     )
+       
 }
 
 export default MealDetailsPage;
